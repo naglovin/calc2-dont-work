80ecab8f03b8658de37abb82fd43937d31c7ba23
Danil <Kutfilloev.danil@yandex.ru>
2022-11-13 14:09:04 +0500

    tired

---
+++ guid.py
@@ -0,0 +1,9 @@
+from div_mod import do_it as div
+from mult_mod import do_it as mul
+from subt_mod import do_it as sub
+from sum_mod import do_it as sum
+
+dict_ar = {'/': div, '*': mul, '-': sub, '+': sum}
+print("/")
+
+dict_log = {'/': 'деление', '*': 'умножение', '-': 'вычитание', '+': 'сложение'}
